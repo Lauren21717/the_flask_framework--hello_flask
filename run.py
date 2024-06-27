9afe9bf7977b5bf38152be3c47d68c2b42c51658
Lauren Yeung <laurenyeung217@gmail.com>
2024-06-27 15:42:16 +0000

    Remove the extra line and update the debug on run.py

--- run.py
+++ run.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP","0.0.0.0"),
         port=int(os.environ.get("PORT","5000")),
-        debug=True
+        debug=False
         #only set debug=True when you developing your project or app due to the security flaw
     )
     
